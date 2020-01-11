b3d416efbc13f744c07335a28fc660a3f670b215
Xavier Davis <xdavis1@icloud.com>
2020-01-10 18:22:56 -0800

    Updates

---
+++ precache-manifest.6a48429b7605b47c78a8ee449526c2d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652490b921f64a65269b30fa91a8b313",
+    "url": "/WebSurfing/index.html"
+  },
+  {
+    "revision": "2baeadabeed39a8e28ce",
+    "url": "/WebSurfing/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "785cc36b9bd9a30ee249",
+    "url": "/WebSurfing/static/js/2.e1630bc7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/WebSurfing/static/js/2.e1630bc7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2baeadabeed39a8e28ce",
+    "url": "/WebSurfing/static/js/main.2cb844ce.chunk.js"
+  },
+  {
+    "revision": "22ce4bfa02e9119f10ce",
+    "url": "/WebSurfing/static/js/runtime-main.fea70a83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WebSurfing/static/media/logo.5d5d9eef.svg"
+  }
+]);
