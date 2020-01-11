85078a2bb76fa8949b132aed3bae7ea2f144dd31
Xavier Davis <xdavis1@icloud.com>
2020-01-10 22:17:15 -0800

    I created page files that were needed as well as comment in each component folder for sample cod e

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,18 @@
+// vvvvvvvv This is just sample code if you dont need it just delete it vvvvvvvvvvv
+
+// import React from "react";
+// import "./style.css";
+
+// // The ...props means, spread all of the passed props onto this element
+// // That way we don't have to define them all individually
+
+// export function Input(props) {
+//     return (
+//       <div className="form-group">
+//         <input className="form-control" {...props} />
+//       </div>
+//     );
+//   }
+
+// vvvvvvv If you do not use export function then you this example down below vvvvvvvv
+// export default DeleteBtn;
