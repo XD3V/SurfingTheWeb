53e19172aa779ee92727cd0bb99009c038e4270f
Xavier Davis <xdavis1@icloud.com>
2020-01-10 20:37:05 -0800

    creating a server.js as well as modifing other file's location so the structure is solid.

---
+++ server.js
@@ -0,0 +1,36 @@
+// Setting up dependencies
+const express = require("express");
+const app = express();
+
+const routes = require("./surfing-the-web/routes");
+
+const PORT = process.env.PORT || 3001;
+
+//Define middleware here
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+//Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes)
+
+// Connect to the Mongo Db
+const mongoose = require("mongoose");
+console.log("Connecting to Mongoose");
+// mongoose.connect(process.env.MONGOD_URI || "mongodb+srv://admin:googleBooksApp@cluster0-2txa4.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Admin:Admin1@cluster0-g2fig.mongodb.net/test?retryWrites=true&w=majority", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+    .then(() => console.log("Connected to MongoDB Atlas"))
+    .catch(err => {
+        console.log(err);
+        console.log("Could not connect");
+    });
+
+// Start the API server
+app.listen(PORT, function(){
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
