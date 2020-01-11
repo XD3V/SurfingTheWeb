a2727e0ff5b37ff37dadeed6b7e40ecf3de1150e
Xavier Davis <xdavis1@icloud.com>
2020-01-10 21:36:21 -0800

    I had to modify const routes required location since it was looking for a file route that did not exist.

--- routes/index.js
+++ routes/index.js
@@ -14,3 +14,13 @@ router.use(function(req, res) {
 });
 
 module.exports = router;
+
+// const fs = require('fs');
+// const path = require('path');
+
+// module.exports = (app) => {
+//   // API routes
+//   fs.readdirSync(__dirname + '/api/').forEach((file) => {
+//     require(`./api/${file.substr(0, file.indexOf('.'))}`)(app);
+//   });
+// };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-const routes = require("./surfing-the-web/routes");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 
