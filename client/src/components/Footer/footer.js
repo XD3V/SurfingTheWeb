09162f87081210019e2549c728f79c58b608a77f
halima todd <halimatodd@gmail.com>
2020-01-15 20:30:31 -0800

    footer

---
+++ client/src/components/Footer/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <div id="container">
+        <div id="main"></div>
+      </div>
+      <footer id="footer">
+        <span>Xavier Davis, Berenice Kelly, Dior Christian, Richard Mazel and Halima Todd </span>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
