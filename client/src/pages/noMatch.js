28829d99e70a5a14711f96ec6e37e109918f5dd7
B Kelly <berenicekelly808@gmail.com>
2020-01-20 18:28:59 -0800

    adding the 404 page

---
+++ client/src/pages/noMatch.js
@@ -0,0 +1,18 @@
+import React from "react";
+//import { Col, Row, Container } from "../components/Card/Cards";
+
+
+function NoMatch() {
+  return (
+    <div className='Container d-flex'>
+      <div className='Row'>
+        <div className='Col-md-12'>
+            <h1>404 Page Not Found</h1>
+            </div>
+            </div>
+            </div>
+            
+  );
+}
+
+export default NoMatch;
