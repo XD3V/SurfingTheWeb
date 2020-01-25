e211f6ffe7cd4c4803898abf5dcdc42e8fa21b6a
B Kelly <berenicekelly808@gmail.com>
2020-01-24 19:14:14 -0800

    remove stick figure

--- client/src/pages/noMatch.js
+++ client/src/pages/noMatch.js
@@ -5,7 +5,6 @@ const NotFound = () => (
 <div style={{margin: 'auto', marginTop: 0}}>
 <center><h1>Oops!</h1></center>
 <center><h2>404 - Page Not Found</h2></center>
-<center><h2>¯\_(ツ)_/¯</h2></center>
 <h2><button style={{padding: 10, margin: 18, borderRadius: 4, borderWidth: 2, borderColor: '#fff', fontSize: 16}}><Link to="/">Return to Home Page</Link></button></h2>
 </div>
 );
