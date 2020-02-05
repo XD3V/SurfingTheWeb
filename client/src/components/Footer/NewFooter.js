6c382747d22f24abfb0139a8992e7d2bd7f14c24
B Kelly <berenicekelly808@gmail.com>
2020-02-04 17:49:43 -0800

    footer icon added

--- client/src/components/Footer/NewFooter.js
+++ client/src/components/Footer/NewFooter.js
@@ -4,7 +4,8 @@ import "./footer.css";
 const Footer = () => {
   return (
     <footer id="page-footer">
-      <h6 class="white-text center-align">Developed by Xavier Davis, Berenice Kelly, Dior Christian, Richard Mazel and Halima Todd</h6>
+    <h6 class="white-text center-align">Developed by Xavier Davis, Berenice Kelly, Dior Christian, Richard Mazel and Halima Todd</h6>
+    <h5 class="white-text center-align"><i class="material-icons small">computer</i></h5>
   </footer>
 
     //old footer:
